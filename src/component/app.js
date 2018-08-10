46df7ab0180744fe1b26a85df0549e351d0e154d
jnetc <apxdemon@gmail.com>
2018-08-10 19:07:59 +0300

    add exaples for fetch & async in class

--- src/component/app.js
+++ src/component/app.js
@@ -5,6 +5,23 @@ class App {
     this.ui = addTxt();
     this.el = document.getElementById('app');
   }
+
+    // Fetch 2015
+  get(url){
+    return new Promise((res, err)=> {
+      fetch(url)
+      .then(response => response.status == 200 ? response.json() : alert('We are sorry, content disconected'))
+      .then(data => res(data))
+      .catch(err => err(err));
+    }) 
+  }
+
+    // Async 2017
+  async getJson(url){
+    let res = await fetch(url);
+    let dataASync = await res.json();
+    return dataASync;
+  }
 }
 export default App;
 
--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import App from './component/app';
 const z = new App();
 const addApp = z.el.appendChild(z.ui);
 console.log('Hello Webpack 4 & add console filter comand from readme.md');
+z.get('./main.json')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+z.getJson('./main.json')
+.then(data => console.log(data))
+.catch(err => console.log(err));
